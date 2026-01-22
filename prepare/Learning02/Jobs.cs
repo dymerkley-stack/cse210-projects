940e0e2f4d4a4e7128dc18250fb6837eca0671d9
Dylan Merkley <your-github-dymerkley@gmail.com>
2026-01-21 17:53:16 -0700

    Finished Code

---
+++ prepare/Learning02/Jobs.cs
@@ -0,0 +1,14 @@
+//this is going to create the job class. 
+//One that stores job names and work dates.
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
