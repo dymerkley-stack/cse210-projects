e78c3a8763eeed0ece6d95f861d9a03e2dcce4e1
Dylan Merkley <your-github-dymerkley@gmail.com>
2026-02-04 17:20:08 -0700

    Add Learning03 fraction project

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,50 @@
+using System.Dynamic;
+namespace Learning03;
+
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public string GetFractionString()
+    {
+        string frac = $"{_top}/{_bottom}";
+        return frac;
+    }
+    public double GetDecimalValue()
+    {
+        double stuff = (double)_top/_bottom;
+        return stuff;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,18 @@
 using System;
-
+using System.Xml.Schema;
+namespace Learning03;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f = new Fraction();
+        for (int i = 0; i < 20; i++){
+            Random rnd = new Random();
+            f.SetTop(rnd.Next(1, 101));
+            f.SetBottom(rnd.Next(1, 101));
+            string frac = f.GetFractionString();
+            double dec = f.GetDecimalValue();
+            Console.WriteLine($"Fraction: String: {frac}, Decimal: {dec}");
+        }
     }
 }
