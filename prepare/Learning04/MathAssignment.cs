60f5ab99c46094236aa64dffac580911c3b65bd3
Dylan Merkley <your-github-dymerkley@gmail.com>
2026-02-18 16:47:44 -0700

    Here's the learning

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,15 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+    public MathAssignment(string name, string topic, string textbook, string problems) 
+    : base (name, topic)
+    {
+        _textbookSection = textbook;
+        _problems = problems;
+    }
+    public string GetHomeworkList()
+    {
+        return $"{_textbookSection} problems {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment me = new Assignment("Dylan", "Programing with Functions");
+        Console.WriteLine(me.GetSummary());
+        
+        MathAssignment math = new MathAssignment("Dylan", "Math", "Section 10.5", "1,3,5,7,9");
+        Console.WriteLine();
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment sux = new WritingAssignment("Dylan", "Market Trends", "AI Copilot Integration");
+        Console.WriteLine();
+        Console.WriteLine(sux.GetSummary());
+        Console.WriteLine(sux.GetWritingInformation());
     }
 }
